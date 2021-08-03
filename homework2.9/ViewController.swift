592e8ec37083186f43694441a0d59de9be27c6d2
Mariia Berger <slackerpanda@gmail.com>
2021-08-03 16:58:45 +0300

    Initial Commit

---
+++ homework2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework2.9
+//
+//  Created by Maria Berger on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
